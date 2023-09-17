0b2977e581fef9c1ac304e7738d5c128001bc084
hanad124 <hanaddaahir124@gmail.com>
2023-09-17 07:16:33 +0300

    Changing clerk authentication to firebase auth, but not of all.

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,24 @@
 
 import Image from "next/image";
 import { UserButton, useUser } from "@clerk/nextjs";
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Sign } from "crypto";
+import { db } from "@/firebase";
 
 export default function Home() {
   const { isSignedIn, user } = useUser();
+
 
   return (
     <>
       {isSignedIn ? (
         <>
           <div className="">Hello, {user.firstName}! Welcome to Clerk</div>
-          {/* <UserButton afterSignOutUrl="/" /> */}
-          <SignOutButton className="bg-red-500" />
+          <h1></h1>
+          <UserButton afterSignOutUrl="/" />
+          {/* <SignOutButton className="bg-red-500" /> */}
         </>
       ) : (
         <>
